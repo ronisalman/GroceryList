990ae9e537e61e3f12a613c474d87e7a2de38782
Roni <ronisalman@email.arizona.edu>
2020-02-24 20:25:24 -0700

    added input field and button

--- GroceryList.js
+++ GroceryList.js
@@ -1,2 +1,13 @@
 // Created by Roni Salman
 
+
+function addingItem(){
+
+	var item = document.querySelector("#itemInput").value;
+	console.log(item);
+
+	var ul = document.querySelector("list");
+	var li = document.createElement("li");
+
+	
+}

308a3c42ea285e5d95e99de90a8ef2bc945a9357
Roni <ronisalman@email.arizona.edu>
2020-02-25 21:00:26 -0700

    manipulating DOM work

--- GroceryList.js
+++ GroceryList.js
@@ -6,8 +6,12 @@ function addingItem(){
 	var item = document.querySelector("#itemInput").value;
 	console.log(item);
 
-	var ul = document.querySelector("list");
+	var ul = document.querySelector("#list");
 	var li = document.createElement("li");
 
-	
+	li.appendChild(document.createElement("Button"));
+	li.appendChild(document.createTextNode(item));
+	li.appendChild(document.createElement("Button"));
+
+	ul.appendChild(li);
 }

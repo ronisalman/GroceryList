cfbacae89d0034cb351a0b8e3777360edc2b4a22
Roni <ronisalman@email.arizona.edu>
2020-02-27 21:08:15 -0700

    changing JS structure

--- GroceryList.js
+++ GroceryList.js
@@ -1,25 +1,3 @@
 // Created by Roni Salman
 
 
-function addingItem(){
-
-	var item = document.querySelector("#itemInput").value;
-
-	var ul = document.querySelector("#list");
-	var li = document.createElement("li");
-
-	var cross = document.createElement("Button");
-	var remove = document.createElement("Button");
-
-	cross.className = "cross";
-	remove.className = "remove";
-
-	li.appendChild(cross);
-	li.appendChild(document.createTextNode(item));
-	li.appendChild(remove);
-
-//.textContent="Delete";
-	ul.appendChild(li);
-
-	document.querySelector(".remove").textContent="Delete";
-}

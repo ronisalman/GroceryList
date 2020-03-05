b0a0745459f94b9399f59b1dcbeb88dcfd18ef31
Roni <ronisalman@email.arizona.edu>
2020-03-04 21:21:51 -0700

    restructure of code

--- GroceryList.js
+++ GroceryList.js
@@ -1,3 +1,21 @@
 // Created by Roni Salman
 
 
+function init(){
+	createDelete();
+}
+
+// Create a "close" button and append it to each list item
+
+function createDelete(){
+
+}
+
+
+// Click on a close button to hide the current list item
+
+
+// Create a new list item when clicking on the "Add" button
+
+
+init();

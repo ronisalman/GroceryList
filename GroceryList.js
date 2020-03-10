990052c15d99a747ca6250b4d6da3a0b8ca3a2b6
Roni <ronisalman@email.arizona.edu>
2020-03-09 20:38:22 -0700

    added delete function

--- GroceryList.js
+++ GroceryList.js
@@ -1,18 +1,27 @@
 // Created by Roni Salman
 
+const li=document.querySelectorAll("li");
+
 
 function init(){
 	createDelete();
 }
 
-// Create a "close" button and append it to each list item
+//creating the delete button with delete ability 
 
 function createDelete(){
+	for(let i=0;i<li.length;i++){
+		let btn=document.createElement("button");
+		btn.innerHTML="Delete";
+		//removing the item clicked
+		btn.addEventListener("click",function(){
+			btn.parentElement.style.display="none";
+		});
 
+		li[i].appendChild(btn);
+	}
 }
 
-
-// Click on a close button to hide the current list item
 
 
 // Create a new list item when clicking on the "Add" button
